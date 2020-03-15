f970bebf6440016e4e8a8ae291f71385aa3f3ef9
apostol <tornado1976@inbox.lv>
2020-03-15 13:56:37 +0200

    First commit

---
+++ Snake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3,3,'#');
+            p1.Draw();
+
+            Point p2 = new Point(5,5,'#');
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            pList.RemoveAt(1);
+
+            Console.ReadLine(); 
+        }
+    }
+}
